a77d9933616be0c90760794fc87bd7cf9700df92
Stefano Simoni <stefano.simoni.10@hotmail.com.ar>
2024-12-30 14:25:40 -0300

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { Icon } from '@iconify/vue'
+
+const app = createApp(App)
+
+app.use(router)
+app.component('Icon', Icon)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>Hello World!</main>
+</template>
+
+<script setup></script>
+
+<style scoped>
+main {
+  @apply text-red-600;
+}
+</style>
